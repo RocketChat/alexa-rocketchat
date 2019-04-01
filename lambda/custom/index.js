443ceb2d6aaf3f433adcf5c0c382922c3aa42e8c
PrajvalRaval <41849970+PrajvalRaval@users.noreply.github.com>
2019-04-01 11:37:19 +0530

    Lambda Starter

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,37 +1,229 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
+
 const Alexa = require('ask-sdk-core');
+const axios = require('axios');
+
+//User Credentials
+const username0 = '';
+const password0 = '';
+const authtoken0 = '';
+const userid0 = '';
+
+//REST API Endpoints
+const loginUrl = 'https://bots.rocket.chat/api/v1/login';
+const createchannelurl = 'https://bots.rocket.chat/api/v1/channels.create';
+const deletechannelurl = 'https://bots.rocket.chat/api/v1/channels.delete';
+const postmessageurl = 'https://bots.rocket.chat/api/v1/chat.postMessage';
+const channelmessageurl = 'https://bots.rocket.chat/api/v1/channels.messages?roomName=';
+
+//Axios Functions
+const login = async () => {
+  try {
+    const { logindata } = await axios.post(loginUrl, {name: username0, password: password0});
+    return logindata;
+  } catch (loginerror) {
+    console.error('cannot fetch quotes', loginerror);
+  }
+};
+
+var headers = {
+            'X-Auth-Token': authtoken0, 
+            'X-User-Id': userid0
+            };
+
+const createChannel = async (channelname0) => {
+  try {
+    const { data } = await axios({
+  method: 'post',
+  url: createchannelurl,
+  data: {
+    name : channelname0
+  },
+  headers: headers
+});
+    return data;
+  } catch (error) {
+    console.error('cannot create channel', error);
+    return error;
+  }
+};
+
+const deleteChannel = async (channelname0) => {
+  try {
+    const { data } = await axios({
+  method: 'post',
+  url: deletechannelurl,
+  data: {
+    roomName : channelname0
+  },
+  headers: headers
+});
+    return data;
+  } catch (error) {
+    console.error('cannot delete channel', error);
+    return error;
+  }
+};
+
+const postMessage = async (channelname0,message) => {
+  try {
+    const { data } = await axios({
+  method: 'post',
+  url: postmessageurl,
+  data: {
+    channel : '#'+channelname0,
+    text: message
+  },
+  headers: headers
+});
+    return data;
+  } catch (error) {
+    console.error('cannot post message', error);
+    return error;
+  }
+};
+
+const channelMessage = async (channelname0) => {
+  try {
+    const { data } = await axios({
+  method: 'get',
+  url: channelmessageurl+channelname0,
+  headers: headers
+});
+    return data;
+  } catch (error) {
+    console.error('cannot get channel message', error);
+    return error;
+  }
+};
+
+
+//Alexa Intent Functions
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Rocket Chat Alexa Skill, you can say hello!';
+  async handle(handlerInput) {
+    
+      const speechText = `Welcome To Rocket.Chat Alexa Skill. What Would you like to do today ?`;
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Rocket Chat', speechText)
-      .getResponse();
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('Welcome To Rocket.Chat', speechText)
+        .getResponse();
+    
   },
 };
 
-const HelloWorldIntentHandler = {
+const CreateChannelHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'createchannel';
   },
-  handle(handlerInput) {
-    const speechText = 'Hello World! Looking forward to the new incredible user experiences in the Alexa ecosystem - powered by Rocket.Chat';
+  async handle(handlerInput) {
+    try {
+      
+      var channelname0 = handlerInput.requestEnvelope.request.intent.slots.channelname.value;
+      
+      const login0 = await login();
+      const rawdata = await createChannel(channelname0);
+      
+      const speechText = ` ${rawdata.success} `;
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Rocket Chat', speechText)
-      .getResponse();
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Create A Channel', speechText)
+        .reprompt(speechText)
+        .getResponse();
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
+
+const DeleteChannelHandler = {
+  canHandle(handlerInput) {
+     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'deletechannel';
+  },
+  async handle(handlerInput) {
+    try {
+      
+      var channelname0 = handlerInput.requestEnvelope.request.intent.slots.channeldelete.value;
+      
+      const login0 = await login();
+      const rawdata = await deleteChannel(channelname0);
+      
+      const speechText = ` ${rawdata.success} `;
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Delete A Channel', speechText)
+        .reprompt(speechText)
+        .getResponse();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+const PostMessageHandler = {
+  canHandle(handlerInput) {
+     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'postmessage';
+  },
+  async handle(handlerInput) {
+    try {
+      
+      var message = handlerInput.requestEnvelope.request.intent.slots.messagepost.value;
+      var channelname0 = handlerInput.requestEnvelope.request.intent.slots.messagechannel.value;
+      
+      const login0 = await login();
+      const rawdata = await postMessage(channelname0,message);
+      
+      const speechText = ` ${rawdata.success} `;
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Post A Message', speechText)
+        .reprompt(speechText)
+        .getResponse();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+const ChannelMessageHandler = {
+  canHandle(handlerInput) {
+     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'channelmessage';
+  },
+  async handle(handlerInput) {
+    try {
+      
+      var channelname0 = handlerInput.requestEnvelope.request.intent.slots.getmessagechannelname.value;
+      
+      const login0 = await login();
+      const rawdata = await channelMessage(channelname0);
+      
+      const speechText = ` ${rawdata.messages[0].u.username} says ${rawdata.messages[0].msg} `;
+
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Channel Message', speechText)
+        .reprompt(speechText)
+        .getResponse();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
@@ -39,12 +231,12 @@ const HelpIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speechText = 'Yet To Write Help Instructions';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Rocket Chat', speechText)
+      .withSimpleCard('Inspiration', speechText)
       .getResponse();
   },
 };
@@ -60,7 +252,7 @@ const CancelAndStopIntentHandler = {
 
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Rocket Chat', speechText)
+      .withSimpleCard('Inspiration', speechText)
       .getResponse();
   },
 };
@@ -95,7 +287,10 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    CreateChannelHandler,
+    DeleteChannelHandler,
+    PostMessageHandler,
+    ChannelMessageHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
