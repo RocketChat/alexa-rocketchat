f036e9d748ee482dfae01886267a9729b37f4f3a
Rocket_Chat <naidu.adarsh2511@gmail.com>
2020-06-06 22:22:27 +0530

    moving get last message from channel intent handler to separate file

--- lambda/custom/utils.js
+++ lambda/custom/utils.js
@@ -4,6 +4,17 @@ const supportsAPL = (handlerInput) => {
 	return aplInterface != null && aplInterface !== undefined;
 };
 
+const supportsDisplay = (handlerInput) => {
+	const hasDisplay =
+		handlerInput.requestEnvelope.context &&
+		handlerInput.requestEnvelope.context.System &&
+		handlerInput.requestEnvelope.context.System.device &&
+		handlerInput.requestEnvelope.context.System.device.supportedInterfaces &&
+		handlerInput.requestEnvelope.context.System.device.supportedInterfaces.Display;
+	return hasDisplay;
+};
+
 module.exports = {
 	supportsAPL,
+	supportsDisplay,
 };
