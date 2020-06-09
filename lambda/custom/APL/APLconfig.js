ea71dd50f7794eb3c45033f59eac57b5e255fd49
Rocket_Chat <naidu.adarsh2511@gmail.com>
2020-06-09 23:18:04 +0530

    making an APLconfig file which contains resources common across many APL templates

---
+++ lambda/custom/APL/APLconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+	logoUrl: 'https://raw.githubusercontent.com/RocketChat/Rocket.Chat.Artwork/master/Logos/icon-circle-1024.png',
+	bg1: 'https://user-images.githubusercontent.com/41849970/60673516-82021100-9e95-11e9-8a9c-cc68cfe5acf1.png',
+	bg2: 'https://user-images.githubusercontent.com/41849970/60651516-fcb23880-9e63-11e9-8efb-1e590a41489e.png',
+	bg3: 'https://user-images.githubusercontent.com/41849970/60644955-126c3180-9e55-11e9-9147-7820655f3c0b.png',
+	bg4: 'https://user-images.githubusercontent.com/41849970/60758741-64b97800-a038-11e9-9798-45b3b8a89b31.png',
+};
