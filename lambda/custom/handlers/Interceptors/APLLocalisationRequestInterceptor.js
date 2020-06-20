032e438e6549542a59afdda1cdf4851da763e7ae
Rocket_Chat <naidu.adarsh2511@gmail.com>
2020-06-20 17:37:21 +0530

    using regexp in custom translate function for replacing parameters with input data

--- lambda/custom/handlers/Interceptors/APLLocalisationRequestInterceptor.js
+++ lambda/custom/handlers/Interceptors/APLLocalisationRequestInterceptor.js
@@ -2,10 +2,12 @@ const { randomProperty } = require('../../utils');
 
 const APLLocalisationRequestInterceptor = {
 	process(handlerInput) {
+		// this is a custom translate function which is added to the handlerInput object
 		handlerInput.translate = (input, data = {}) => {
 			try {
 				// import the resource specific locale file
 				const resourceFilePath = `../../resources/${ handlerInput.requestEnvelope.request.locale }.json`;
+				// eslint-disable-next-line no-unused-vars
 				const resource = require(resourceFilePath);
 
 				// get the value of the requested input key
@@ -22,7 +24,8 @@ const APLLocalisationRequestInterceptor = {
 				// replace the parameters in the value with input data
 				let key; let value;
 				for ([key, value] of Object.entries(data)) {
-					output = output.replace(`{${ key }}`, value);
+					const re = new RegExp(`{${ key }}`, 'g');
+					output = output.replace(re, value);
 				}
 				return output;
 
