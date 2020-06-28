24132cdfe630fd02e1011874fb2225bd30686883
Adarsh Naidu <naidu.adarsh2511@gmail.com>
2020-06-28 19:09:43 +0530

    creating an apl template

---
+++ lambda/custom/APL/templates/burgerTemplate.js
@@ -0,0 +1,51 @@
+const layout = require('../layouts/burgerLayout.json');
+const APLconfig = require('../APLconfig');
+
+const template = (top, middle, bottom) => ({
+	type: 'Alexa.Presentation.APL.RenderDocument',
+	version: '1.0',
+	document: layout,
+	datasources: {
+		DeleteChannelPageData: {
+			type: 'object',
+			objectId: 'rcDeleteChannel',
+			backgroundImage: {
+				contentDescription: null,
+				smallSourceUrl: null,
+				largeSourceUrl: null,
+				sources: [
+					{
+						url: APLconfig.bg2,
+						size: 'small',
+						widthPixels: 0,
+						heightPixels: 0,
+					},
+					{
+						url: APLconfig.bg2,
+						size: 'large',
+						widthPixels: 0,
+						heightPixels: 0,
+					},
+				],
+			},
+			textContent: {
+				placeholder: {
+					type: 'PlainText',
+					text: top,
+				},
+				channelname: {
+					type: 'PlainText',
+					text: middle,
+				},
+				successful: {
+					type: 'PlainText',
+					text: bottom,
+				},
+			},
+			logoUrl: APLconfig.logoUrl,
+		},
+
+	},
+});
+
+module.exports = template;
